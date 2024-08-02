5a040ad5c93136d1715223c088007a26f714547e
Thiago Boccalon <thiago_rodrigo90@hotmail.com>
2024-08-02 19:23:10 +0100

    Starting my DbContext in Infrasstructure.

---
+++ LibraryManagement.Infrastructure/Persistence/LibraryManagementDbContext.cs
@@ -0,0 +1,21 @@
+﻿using LibraryManagement.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Infrastructure.Persistence
+{
+    public class LibraryManagementDbContext : DbContext
+    {
+        public LibraryManagementDbContext(DbContextOptions<LibraryManagementDbContext> options) : base(options)
+        {
+            
+        }
+        public List<Book> Books { get; set; }
+        public List<User> Users { get; set; }
+        public List<Loan> Loans { get; set; }
+    }
+}
